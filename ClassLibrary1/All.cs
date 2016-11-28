9f151eb70d96e95c0cfaea58d86a7eb75d54dd71
Angel Murchison <angelmurchison09@gmail.com>
2016-11-28 15:50:07 -0500

    created human,robot,panda classes. created displayname, displaygreeting methods, created startup and shutdown methods.

---
+++ ClassLibrary1/All.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansPandasAndRobots
+{
+    public abstract class HumansPandasAndRobots
+    {
+        public string DisplayName(string name)
+        {
+            string Name = $"{name}";
+            return Name;
+        }
+        public abstract void DisplayGreeting();
+    }
+}
---
+++ ClassLibrary1/Robots.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansPandasAndRobots
+{
+    public class Robots
+    {
+        public void startup()
+        {
+            bool active = true;
+            Console.WriteLine("Started..");
+        }
+        public void shutdown()
+        {
+            bool active = false;
+            Console.WriteLine("Shutdown..");
+        }
+    }
+}
